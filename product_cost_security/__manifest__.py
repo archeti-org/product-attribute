489acb7713006d18cf762d4006ea4ea0190e3a79
Hugo Meunier <hmeunier@archeti.com>
2026-08-07 13:42:02 -0400

    [42676][MIG] product_cost_security : debbug view

--- product_cost_security/__manifest__.py
+++ product_cost_security/__manifest__.py
@@ -13,5 +13,8 @@
     "application": False,
     "installable": True,
     "depends": ["product"],
-    "data": ["security/product_cost_security.xml", "views/product_views.xml"],
+    "data": [
+        "security/product_cost_security.xml",
+         #"views/product_views.xml"
+    ],
 }
